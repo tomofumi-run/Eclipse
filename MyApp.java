eb5f8aa05deeac4dd9bae0950027b46f6bbff281
tomofumi-run <nnsu_1014@yahoo.co.jp>
2021-07-11 07:52:44 +0900

    [p]7/11

--- MyApp.java
+++ MyApp.java
@@ -1,6 +1,12 @@
 public class MyApp {
   public static void main(String[] args){
     String msg = "Hello Message";
+    char a = 'a' // 1文字
+    int x = 10; // 整数
+    long y = 5555555555L; //末尾にL
+    double d = 2343.234 //浮動小数
+
     System.out.println(msg);
+
   }
 }
